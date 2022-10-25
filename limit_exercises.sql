07a04e907309de9c726fff5457b986d7390770e7
Madgalena Rahn <magdalena.m.rahn@gmail.com>
2022-10-25 14:28:06 -0500

    Adding updated Limit__Exercises with a few corrections.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,4 +1,5 @@
 USE employees;
+-- example :
 SELECT DISTINCT title FROM titles;
 
 /* 2. List the first 10 distinct last name sorted in descending order. 
@@ -14,7 +15,9 @@ Zschoche
 Zongker
 */
 SELECT DISTINCT last_name FROM employees
-ORDER BY last_name DESC;
+ORDER BY last_name DESC
+LIMIT 10;
+
 
 /* 3. Find all previous or current employees hired in the 90s and born on Christmas. Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. 
 Write a comment in your code that lists the five names of the employees returned :
@@ -34,4 +37,6 @@ SELECT * FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 	AND birth_date LIKE '%%%%-12-25'
 		ORDER BY hire_date
-		LIMIT 5 OFFSET 50;
+		LIMIT 5 OFFSET 45;
+		-- offsetting the first 9 pages, in order to get to the 10th page.
+		
