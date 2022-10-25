48dccf7db9ae280d899b10e5d01f93ca6dc224af
Madgalena Rahn <magdalena.m.rahn@gmail.com>
2022-10-25 17:07:38 -0500

    Adding updated file.

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,15 +1,3 @@
-/* Find the smallest and largest current salary from the salaries table.
-
-Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like: 
-
-USE saas_llc;
-SELECT city, street_name, state, zip, 
-CONCAT('0', ZIP) AS full_zip,
-LOWER (street_name) AS street,
-REPLACE (state, 'A', 'assachusetts') AS full_state -- note that the replace-function is case-sensitive
-FROM customer_details
-ORDER BY street_name; */
-
 /* Write a query to to find all employees whose last name starts and ends with 'E'. Use concat() to combine their first and last name together as a single column named full_name. */
 
 USE employees;
@@ -30,3 +18,13 @@ WHERE last_name LIKE 'E%E' ;
 SELECT datediff(curdate(), birth_date) from employees
 	WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
 	
+
+/* Find the smallest and largest current salary from the salaries table. */
+describe salaries;
+SELECT MAX(salary), MIN(salary) FROM salaries;
+
+
+/* Use your knowledge of built-in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employee's first name, the first 4 characters of the employee's last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. */
+SELECT *,
+	LOWER(CONCAT(SUBSTR(first_name,1,1), SUBSTR(last_name, 1,4), '_', SUBSTR(birth_date, 6,2), SUBSTR(birth_date, 3,2))) AS username 
+ FROM employees;
