bf67bc0d11d0cc8007b852fdf9f7594ac79163ff
Madgalena Rahn <magdalena.m.rahn@gmail.com>
2022-10-26 14:50:46 -0500

    Adding updated exercises on SQL Group By function.

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -47,7 +47,58 @@ ORDER BY first_name ASC;
 There are 6 duplicate usernames.
 */
 SELECT first_name, last_name, birth_date, count(*),
-	LOWER(CONCAT(SUBSTR(first_name,1,1), SUBSTR(last_name, 1,4), '_', SUBSTR(birth_date, 6,2), SUBSTR(birth_date, 3,2))) AS username 
+	LOWER(CONCAT(SUBSTR(first_name,1,1), SUBSTR(last_name, 1,4), '_', SUBSTR(birth_date, 6,2), SUBSTR(birth_date, 3,2))) 
+	AS username 
 FROM employees
 GROUP BY first_name, last_name, birth_date
 ORDER BY COUNT(*) DESC;
+
+
+SELECT CONCAT(LOWER(SUBSTR(first_name, 1, 1)), LOWER(SUBSTR(last_name, 1, 4)), "_", SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2)) 
+	AS user_id, 
+	COUNT(*) AS amount
+FROM employees
+GROUP BY user_id 
+HAVING amount > 1;
+
+
+SELECT LOWER(CONCAT(SUBSTR(first_name, 1, 1), SUBSTR(last_name, 1, 4), '_', SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2))) 
+	AS username, 
+	COUNT(*) AS amount
+FROM employees
+GROUP BY username
+HAVING amount > 1
+ORDER BY amount DESC;
+
+
+/* 9. Bonus: More practice with aggregate functions:
+a. Determine the historic average salary for each employee. When you hear, read, or think "for each" with regard to SQL, you'll probably be grouping by that exact column. 
+*/
+use salaries;
+describe salaries;
+
+SELECT DISTINCT emp_no, salary, AVG (salary) FROM salaries
+GROUP BY emp_no, salary;
+
+/* 9b. Using the dept_emp table, count how many current employees work in each department. The query result should show 9 rows, one for each department and the employee count. 
+*/
+use employees;
+use dept_emp;
+describe dept_emp;
+
+SELECT dept_no, COUNT(dept_no) AS Departements
+FROM dept_emp
+GROUP BY dept_no 
+ORDER BY Departements;
+-- ...
+
+/* 9d, 9e. Determine how many different salaries each employee has had. This includes both historic and current. 
+*/
+
+use salaries;
+describe salaries;
+
+SELECT emp_no, MIN(salary), MAX(salary)
+FROM salaries
+GROUP BY emp_no, salary
+ORDER BY emp_no;
